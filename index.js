86de9e4cd35295be2458c3bdc37dda1be10f45a9
rosyjackson <rosyjackson2004@gmail.com>
2018-06-20 20:32:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const companyName = 'Scuber';
 
-console.log(companyName);
 
 let mostProfitableNeighborhood = 'Chelsea';
 
-console.log(mostProfitableNeighborhood);
+
 
 let companyCeo = 'Susan Smith';
 
-console.log(companyCeo);
